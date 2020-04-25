8f6f64ab409eae63a455ac5b60eb16585cff3f73
coldboy002 <coldboy002@gmail.com>
2020-04-25 07:55:20 +0700

    ✨ feat: convert redux to global state with context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import ShopContext from './shop';
+
+class GlobalState extends Component {
+  state = {
+    cart: []
+  };
+
+  addProductToCart = product => {
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === product.id
+    );
+
+    if (updatedItemIndex < 0) {
+      updatedCart.push({ ...product, quantity: 1 });
+    } else {
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity++;
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    setTimeout(() => {
+      this.setState({ cart: updatedCart });
+    }, 700);
+  };
+
+  removeProductFromCart = productId => {
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === productId
+    );
+
+    const updatedItem = {
+      ...updatedCart[updatedItemIndex]
+    };
+    updatedItem.quantity--;
+    if (updatedItem.quantity <= 0) {
+      updatedCart.splice(updatedItemIndex, 1);
+    } else {
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+    setTimeout(() => {
+      this.setState({ cart: updatedCart });
+    }, 700);
+  };
+
+  render() {
+    return (
+      <ShopContext.Provider
+        value={{
+          products: this.state.products,
+          cart: this.state.cart,
+          addProductToCart: this.addProductToCart,
+          removeProductFromCart: this.removeProductFromCart
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
+  }
+}
+
+export default GlobalState;
