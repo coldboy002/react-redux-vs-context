5c01d42376fbd6256a752975cc555c01f506bb55
coldboy002 <coldboy002@gmail.com>
2020-04-25 08:54:59 +0700

    🐛 fix: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Provider } from "react-redux";
-import configureStore from "./configureStore.js";
 import "./index.css";
 import App from "./App";
 
-const store = configureStore();
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
